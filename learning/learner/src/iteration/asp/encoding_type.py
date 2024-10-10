1a5003fb701942ac474801ebee3f0d3506c02440
Blai Bonet <bonetblai@gmail.com>
2024-10-10 23:47:56 +0200

    Add LTL encoding type

--- learning/learner/src/iteration/asp/encoding_type.py
+++ learning/learner/src/iteration/asp/encoding_type.py
@@ -5,6 +5,8 @@ class EncodingType(Enum):
     """
     D2 is the encoding related to Francès et al. (AAAI2021): https://arxiv.org/abs/2101.00692
     EXPLICIT is the encoding from Drexler et al. (ICAPS2022): https://arxiv.org/abs/2203.14852
+    D2_LTL is the encoding ...
     """
     D2 = "d2"
     EXPLICIT = "explicit"
+    D2_LTL = "d2_ltl"
